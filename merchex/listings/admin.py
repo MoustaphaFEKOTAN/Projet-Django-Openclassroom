5d8dd64a179ef81865ddade5ee8d0bfc3cb4684f
Moustapha FEKOTAN <moustphafek@gmail.com>
2025-09-09 22:34:03 +0100

    Affichage des nom et d'autres champs des objets au lieu de leur id avec list_display

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -3,5 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from listings.models import Band,Listing
 
-admin.site.register(Band)
+
+class BandAdmin(admin.ModelAdmin):  # nous insérons ces deux lignes..
+ list_display = ('name', 'year_formed', 'genre') # liste les champs que nous voulons sur l'affichage de la liste
+
+admin.site.register(Band, BandAdmin) # nous modifions cette ligne, en ajoutant un deuxième argument
+
 admin.site.register(Listing)
