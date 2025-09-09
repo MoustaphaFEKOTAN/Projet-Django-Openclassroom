1b93a15ba58e1d993abb4a52b7989e5de5e1d2b3
Moustapha FEKOTAN <moustphafek@gmail.com>
2025-09-09 17:32:21 +0100

    Mise en place des gabarits

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -4,28 +4,16 @@ from listings.models import Band,Listing
 
 def hello(request):
     bands = Band.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello Django !</h1>
-        <p>Mes groupes préférés sont :<p>
-        <ul>
-            <li>{bands[0].name}</li>
-            <li>{bands[1].name}</li>
-            <li>{bands[2].name}</li>
-        </ul>
-""")
+    return render(request,
+    'bands/hello.html',
+    {'bands': bands})
 
 
 def articles(request):
     articles = Listing.objects.all()
-    return HttpResponse(f"""
-       
-        <p>Nos articles :<p>
-        <ul>
-            <li>{articles[0].title}</li>
-            <li>{articles[1].title}</li>
-            <li>{articles[2].title}</li>
-        </ul>
-""")
+    return render(request,
+    'listings/article.html',
+    {'articles': articles})
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
