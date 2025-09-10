4fb141465cdb9f1b8c7219b4398b212766a4c15a
Moustapha FEKOTAN <moustphafek@gmail.com>
2025-09-10 22:20:40 +0100

    Methode d'ajout de groupe en étendant le model dans le form

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from listings.models import Band,Listing
 from django.http import Http404
-from listings.forms import ContactUsForm
+from listings.forms import ContactUsForm,BandForm
 from django.core.mail import send_mail
 from django.shortcuts import redirect
 
@@ -66,9 +66,22 @@ def contact(request):
             'listings/contact.html',
             {'form': form})
 
-def band_cretae(request):
+def band_create(request):
+    if request.method == 'POST':
+        form = BandForm(request.POST)
+        if form.is_valid():
+            # créer une nouvelle « Band » et la sauvegarder dans la db
+            band = form.save()
+            # redirige vers la page de détail du groupe que nous venons de créer
+            # nous pouvons fournir les arguments du motif url comme arguments à la fonction de redirection
+            return redirect('band-detail', band.id)
+
+    else:
+        form = BandForm()
+
     return render(request,
-    'bands/bande_create.html') 
+            'bands/band_create.html',
+            {'form': form})
 
 
 def articles(request):
