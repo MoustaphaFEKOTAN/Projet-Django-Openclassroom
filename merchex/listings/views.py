e7ffce0802960c4eebdae045c813c0d58074ec1d
Moustapha FEKOTAN <moustphafek@gmail.com>
2025-09-10 20:56:53 +0100

    Mise en place d'un formulaire de contact

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from listings.models import Band,Listing
 from django.http import Http404
 from listings.forms import ContactUsForm
+from django.core.mail import send_mail
 
 def band_list(request):
     bands = Band.objects.all()
@@ -38,11 +39,29 @@ def band_listing(request, band_id):
 
 
 
+
 def contact(request):
-  form = ContactUsForm()  # ajout d’un nouveau formulaire ici
-  return render(request,
-          'listings/contact.html',
-          {'form': form})  # passe ce formulaire au gabarit
+    if request.method == 'POST':
+        # créer une instance de notre formulaire et le remplir avec les données POST
+        form = ContactUsForm(request.POST)
+
+        if form.is_valid():
+            send_mail(
+            subject=f'Message from {form.cleaned_data["name"] or "anonyme"} via MerchEx Contact Us form',
+            message=form.cleaned_data['message'],
+            from_email=form.cleaned_data['email'],
+            recipient_list=['admin@merchex.xyz'],
+        )
+    # si le formulaire n'est pas valide, nous laissons l'exécution continuer jusqu'au return
+    # ci-dessous et afficher à nouveau le formulaire (avec des erreurs).
+
+    else:
+    # ceci doit être une requête GET, donc créer un formulaire vide
+          form = ContactUsForm()
+
+    return render(request,
+            'listings/contact.html',
+            {'form': form})
 
 def articles(request):
     articles = Listing.objects.all()
